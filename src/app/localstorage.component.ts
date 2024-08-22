ad501ac242ee20dc638ce0651c64d430d632115e
AlienDon <Edon.shala@gmail.com>
2024-08-22 20:45:33 +0200

    ES/NA-009 added validation and styling

--- src/app/localstorage.component.ts
+++ src/app/localstorage.component.ts
@@ -1,19 +1,20 @@
 import { Note } from '../notes';
 
 export class LocalStorage {
-  setLocalStorage(notes: Note[]) {
-    localStorage.setItem('notes', JSON.stringify(notes));
-  }
+	setLocalStorage(notes: Note[]) {
+		localStorage.setItem('notes', JSON.stringify(notes));
+	}
 
-  getLocalStorage() {
-    const localStorageStoredNote = localStorage.getItem('notes');
-    let result: Note[] = [];
-    if (localStorageStoredNote) {
-      result = JSON.parse(localStorageStoredNote);
-    }
-    return result;
-  }
-  clearLocalStorage() {
-    localStorage.clear();
-  }
+	getLocalStorage() {
+		const localStorageStoredNote = localStorage.getItem('notes');
+		let result: Note[] = [];
+		if (localStorageStoredNote) {
+			result = JSON.parse(localStorageStoredNote);
+		}
+		return result;
+	}
+
+	clearLocalStorage() {
+		localStorage.clear();
+	}
 }
