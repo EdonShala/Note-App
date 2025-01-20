0b652ca5c4633a24fda45cbb2e957eed685cbe2d
EdonShala <Edon.shala@gmail.com>
2025-01-20 12:09:58 +0100

    cleanup

--- src/app/shared/services/note.service.ts
+++ src/app/shared/services/note.service.ts
@@ -1,9 +1,9 @@
-import { inject, Injectable } from "@angular/core";
-import { NoteDto } from "../note.dto";
-import { LocalStorageService } from "./localstorage.service";
-import { NotificationService } from "./notification.service";
+import { inject, Injectable } from '@angular/core';
+import { NoteDto } from '../note.dto';
+import { LocalStorageService } from './localstorage.service';
+import { NotificationService } from './notification.service';
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class NoteService {
 	private localStorageService: LocalStorageService = inject(LocalStorageService);
 	public notificationService: NotificationService = inject(NotificationService);
@@ -17,28 +17,28 @@ export class NoteService {
 	}
 
 	save(note: NoteDto): void {
-		const notes = this.getAll();
-		const existingNoteIndex = notes.findIndex(n => n.id === note.id);
+		const notes: NoteDto[] = this.getAll();
+		const existingNoteIndex: number = notes.findIndex(n => n.id === note.id);
 
 		if (existingNoteIndex > -1) {
 			notes[existingNoteIndex] = note;
-			this.notificationService.setNotification("Note successfully updated.");
+			this.notificationService.setNotification('Note successfully updated.');
 		} else {
 			notes.push(note);
-			this.notificationService.setNotification("Note successfully added.");
+			this.notificationService.setNotification('Note successfully added.');
 		}
 
 		this.localStorageService.set(notes);
 	}
 
 	delete(id: string): void {
-		const notes = this.getAll().filter(note => note.id !== id);
+		const notes: NoteDto[] = this.getAll().filter(note => note.id !== id);
 		this.localStorageService.set(notes);
-		this.notificationService.setNotification("Note successfully deleted.");
+		this.notificationService.setNotification('Note successfully deleted.');
 	}
 
 	deleteAll(): void {
 		this.localStorageService.set([]);
-		this.notificationService.setNotification("All notes were successfully deleted.");
+		this.notificationService.setNotification('All notes were successfully deleted.');
 	}
 }
